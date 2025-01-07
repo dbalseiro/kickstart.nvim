f8aa1abd4fbc029efdc58c01df9b2783724393c8
Diego Balseiro <dbalseiro@stackbuilders.com>
2025-01-07 14:24:46 -0500

    Add git integration

--- init.lua
+++ init.lua
@@ -960,6 +960,7 @@ require('lazy').setup({
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
   require 'custom.plugins.terminal',
+  require 'custom.plugins.git',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
